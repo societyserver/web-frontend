b48670fa122b5016efe5622fa5fd03d0d5a22713
Chanaka De Silva <chanu1993@gmail.com>
2016-09-14 10:13:55 +0200

    Fixed issues in login function

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -1,11 +1,24 @@
 angular.module('steam')
 
-  .controller('loginCtrl', ['$rootScope', '$scope', 'handler', '$state', 'localStorageService',
-    function ($rootScope, $scope, handler, $state, localStorageService) {
+  .controller('loginCtrl', ['$rootScope', '$scope', 'handler', '$state', 'localStorageService','steam',
+    function ($rootScope, $scope, handler, $state, localStorageService,steam) {
     $scope.logIn = function () {
       $rootScope.loading = true
       handler.login($scope.signInUsername, $scope.signInPasswd).then(function (response) {
         $state.go('workarea.list')
       })
     }
+
+$scope.user = {};
+
+	$scope.makeLogin = function () {
+            console.log($scope.user);
+            steam.login($scope.user.username, $scope.user.password).then(function (data) {
+                console.log("Response : ", data);
+                var checkLoggedStatus = steam.loginp();
+                console.log("New logged : ", checkLoggedStatus);
+                $state.go('workarea.list');
+            });
+        };
+
   }])
