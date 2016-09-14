80320ed2a1e6c92e520be1e8ec5af9bee951b1cb
Chanaka De Silva <chanu1993@gmail.com>
2016-09-14 10:27:13 +0200

    Clean up registration

--- controllers/registerCtrl.js
+++ controllers/registerCtrl.js
@@ -1,12 +1,15 @@
 angular.module('steam')
 
-  .controller('registerCtrl', ['$rootScope', '$scope', 'handler', '$state', 'localStorageService',
-    function ($rootScope, $scope, handler, $state, localStorageService) {
-    $scope.signUp = function () {
-      $rootScope.loading = true
-      $scope.steamGrp = "techgrind"
-      handler.signup($scope.signUpEmailid, $scope.signUpPasswd, $scope.signUpPasswd2, $scope.steamGrp).then(function (response) {
-        $state.go('workarea.list')
-      })
-    }
+  .controller('registerCtrl', ['$rootScope', '$scope', 'handler', '$state', 'localStorageService','steam',
+    function ($rootScope, $scope, handler, $state, localStorageService, steam) {
+	$scope.registerdata = {};
+	$scope.registerMember = function () {
+	    $scope.registerdata.group = 'steam';
+	    $scope.registerdata.userid = $scope.registerdata.fullname.toLowerCase().replace(/[^a-z ]/g, "").trim().replace(/\s+/g, ".");
+	    return steam.post('register', $scope.registerdata).then(handle_request);
+	};
+	handle_request = function (data) {
+	    return $scope.data = data;
+	};
+
   }])
